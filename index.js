e4476e668005fa3dd6b9737b5a9ca8ddf6c04d2d
Mark Anthony <mr.markgutierrez@gmail.com>
2024-11-19 20:38:45 -0500

    Added water styles to all 3 files with DOM manipulation

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ let boat = '🚢';
 
 for (let x = 0; x < 10; x++) {
     let string = '';
+    let h2 = document.createElement('h2');
     for (let y = 0; y < x; y++) {
         string += boat;
     }
-    console.log(string)
+    h2.innerText = string;
+    let sea = document.getElementById('sea')
+    sea.append(h2)
 }
 
